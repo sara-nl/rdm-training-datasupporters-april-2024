9c395d14cde0a981543fab819d5a01bbba46e1d0
Maithili Kalamkar <maithili86@gmail.com>
2024-03-21 13:09:06 +0100

    Update jobscript.sh

--- Analysis-and-compute-flows/jobscript.sh
+++ Analysis-and-compute-flows/jobscript.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# Set job requirements
+
+# SBATCH -p short
+# SBATCH -t 4:00
+# SBATCH --partition=thin_course
+
+# Change these variables if you want to change the input or output directories
+inputdir="$HOME/input"
+outputdir="$HOME/result"
+resultsfile="result-$SLURM_JOBID.txt"
+
+# Make sure that the outputdir exist
+mkdir -p $outputdir
+
+# Count the words of all files in the inputdir and write to outputdirectory
+# The freuqency table will be sorted on most common words first.
+cat $inputdir/* | tr '[:upper:]' '[:lower:]' | awk '{for(i=1;i<=NF;i++) count[$i]++} END {for(j in count) print j, count[j]}' | sort -k2 -n -r > $outputdir/$resultsfile
